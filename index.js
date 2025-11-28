f6ddfea4363a931c6775129b5384cdde3bc5e023
mohammadnobin <mdnobin.fdr@gmail.com>
2025-11-28 15:59:07 +0600

    refactor: add CORS middleware to enhance API accessibility

--- index.js
+++ index.js
@@ -141,12 +141,13 @@
 require('dotenv').config();
 const mysql = require("mysql2");
 const express = require('express');
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-
+app.use(cors());
 app.use(express.json());
 
 // Database connection for Aiven
